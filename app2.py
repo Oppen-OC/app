b1acbffeed4111de6aa9adc946f7fa081c90d63d
dalpenes@proyeco.es <dalpenes@proyeco.es>
2024-09-09 13:58:58 +0200

    test

--- app2.py
+++ app2.py
@@ -31,16 +31,8 @@ from langchain.retrievers import ParentDocumentRetriever
 from langchain.storage import InMemoryStore
 from langchain.schema import Document
 from langchain_community.docstore.in_memory import InMemoryDocstore
-from sentence_transformers import CrossEncoder
-from langchain.output_parsers.openai_tools import JsonOutputToolsParser
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnableLambda
-from langchain.chains import create_extraction_chain
-from typing import Optional, List
-from langchain.chains import create_extraction_chain_pydantic
-from langchain_core.pydantic_v1 import BaseModel
-from langchain import hub
+from langchain_experimental.text_splitter import SemanticChunker
+
 
 # Load environment variables
 load_dotenv()
@@ -93,43 +85,6 @@ qa_prompt = ChatPromptTemplate.from_messages(
 question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
 
 store = {}
-
-obj = hub.pull("wfh/proposal-indexing")
-llm = ChatOpenAI(model='gpt-3.5-turbo')
-runnable = obj | llm
-
-class Sentences(BaseModel):
-    sentences: List[str]
-    
-# Extraction
-extraction_chain = create_extraction_chain_pydantic(pydantic_schema=Sentences, llm=llm)
-def get_propositions(text):
-    runnable_output = runnable.invoke({
-    	"input": text
-    }).content
-    propositions = extraction_chain.invoke(runnable_output)["text"][0].sentences
-    return propositions
-    
-paragraphs = text.split("\n\n")
-text_propositions = []
-for i, para in enumerate(paragraphs[:5]):
-    propositions = get_propositions(para)
-    text_propositions.extend(propositions)
-    print (f"Done with {i}")
-
-print (f"You have {len(text_propositions)} propositions")
-print(text_propositions[:10])
-
-print("#### Agentic Chunking ####")
-
-from agentic_chunker import AgenticChunker
-ac = AgenticChunker()
-ac.add_propositions(text_propositions)
-print(ac.pretty_print_chunks())
-chunks = ac.get_chunks(get_type='list_of_strings')
-print(chunks)
-documents = [Document(page_content=chunk, metadata={"source": "local"}) for chunk in chunks]
-rag(documents, "agentic-chunks")
 
 def get_history_aware_ret(retriever):
     history_aware_retriever = create_history_aware_retriever(
